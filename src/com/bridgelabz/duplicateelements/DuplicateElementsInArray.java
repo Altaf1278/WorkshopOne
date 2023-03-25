1abbc2b574e917a3c9fdd2808773c0e890dc10b5
altaf <altaf98khan66@gmail.com>
2023-03-25 12:21:01 +0530

    Added: An Array Program To Remove Duplicates In Sorted Array

---
+++ src/com/bridgelabz/duplicateelements/DuplicateElementsInArray.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.duplicateelements;
+
+public class DuplicateElementsInArray {
+	public static void main(String[] args) {
+		System.out.println("Welcome to remove duplicates in a sorted array");
+	}
+
+}
